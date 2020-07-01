0e06e54e4ae08cc60a31f6f863017075f1f9c91d
Holly Baker <hmaybaker@icloud.com>
2020-06-30 23:40:44 -0400

    finished account information form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -56,7 +56,8 @@ class AccountInformationForm extends Component {
                 placeholder='Zipcode'
                 name='zipcode'
                 component= {FormInput}/>
-               
+
+                <div className='account-information-form__line'></div>
 
             {   this.state.showPasswords ? 
                     [
@@ -77,7 +78,20 @@ class AccountInformationForm extends Component {
                     title='Confirm Password'
                     placeholder='Confirm Password'
                     name='confirm'
-                    component= {FormInput}/>
+                    component= {FormInput}/>,
+                    <Field key={3} className='account-information-form__update-information'
+                    onClick={() => this.setState({ showPasswords: false })}
+                    type='button'
+                    title='Update Information'
+                    name='update-information'
+                    component={FormButton}/>,
+                    <Field key={4} className='account-information-form__cancel'
+                    onClick={() => this.setState({ showPasswords: false })}
+                    type='button'
+                    title='Cancel'
+                    name='cancel'
+                    short={true}
+                    component={FormButton}/>
                     ]
                     :
                     <Field className='account-information-form__change-password'

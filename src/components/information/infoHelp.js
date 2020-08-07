eb76a2065a114bc6b47b08f14156e10185ac9da1
Holly Baker <hmaybaker@icloud.com>
2020-08-07 16:40:22 -0400

    built order summary component, infotitle and underlined title components

---
+++ src/components/information/infoHelp.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export function UnderlinedTitle({ className, title }) {
+    return (
+        <div className={`${className} underlined-title`}>
+            {title}
+        </div>
+    )
+}
+
+export function InfoTitle({ className, title, value }) {
+    return (
+        <div className={`${className} into-title`}>
+            <div className='info-title__title'>
+                {title}
+            </div>
+            <div className='info-title__value'>
+                {value}
+            </div>
+        </div>
+    )
+}
